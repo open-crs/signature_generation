0e52f69bfa5dd70526724c322b3959af1c8d9917
Ştefan Pană <stefanpana00@gmail.com>
2023-04-11 19:56:12 +0300

    Delete .hooking.mod.cmd

--- .hooking.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/feather/student/licenta/syscall_hooking/hooking.mod := { echo  /home/feather/student/licenta/syscall_hooking/hooking.o;  echo; } > /home/feather/student/licenta/syscall_hooking/hooking.mod
